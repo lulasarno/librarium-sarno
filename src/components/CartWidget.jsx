47963ec16e0af1f272c7cfad6d93f3202dee2fc6
lulasarno <lulasarnounab@gmail.com>
2025-02-25 00:45:17 -0300

    arreglo img

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,7 +2,7 @@ const CartWidget = () => {
     return(
         <div className="carrito-container"> 
             <span>
-                <img src="../carrito-de-compras.png" alt="" className="carrito-logo"/>
+                <img src="./carrito-de-compras.png" alt="" className="carrito-logo"/>
             </span>
             <span> 5 </span>
         </div>
